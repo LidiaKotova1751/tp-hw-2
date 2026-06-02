4e2e8599c45b36eabf17ea61b9c0809c7ddc6be5
Крылова Лидия <lbkrylova@edu.hse.ru>
2026-06-02 12:01:31 +0300

    Добавление тестов из заданий 2.2 и 2.3 в файл test_recipes.py

--- test_recipes.py
+++ test_recipes.py
@@ -1,4 +1,4 @@
-# Тесты для класса Ingredient, задание 2.1
+# Тесты для класса Ingredient. Задание 2.1
 from Classes import Ingredient
 def test_ingrefient_initialization():
     '''Проверка правильной инициализации атрибутов name, quantity, unit'''
@@ -30,7 +30,7 @@ def test_ingrefient_eq():
     assert  ing_1 != ing_4
 
 
-# Тесты для  класса Recipe, задание 2.2
+# Тесты для класса Recipe. Задание 2.2
 import pytest
 from Classes import Recipe
 def test_recipe_initialization():
@@ -98,3 +98,73 @@ def test_recipe_len():
 
     assert len(recipe) == 2
 
+# Тесты для класса ShoppingList. Задание 2.3
+from Classes import ShoppingList
+def test_shoppinglist_add_recipe():
+    '''Проверка метода add_recipe'''
+    recipe = Recipe('Омлет', [Ingredient('Яйцо', 2, "шт"), Ingredient('Молоко', 50, 'мл')])
+    sl = ShoppingList()
+
+    sl.add_recipe(recipe, 2)
+    res = sl.get_list()
+
+    assert len(res) == 2
+
+    with pytest.raises(ValueError):
+        sl.add_recipe(recipe, 0)
+    with pytest.raises(ValueError):
+        sl.add_recipe(recipe, 0)
+
+def test_shoppinglist_remove_recipe():
+    '''Проверка метода remove_recipe'''
+    recipe1 = Recipe('Тесто', [Ingredient('Мука', 500.0, 'г')])
+    recipe2 = Recipe('Омлет', [Ingredient('Яйцо', 2, 'шт')])
+
+    sl = ShoppingList()
+    sl.add_recipe(recipe1, 1)
+    sl.add_recipe(recipe2, 1)
+
+    sl.remove_recipe('Тесто')
+    res = sl.get_list()
+    assert len(res) == 1
+    assert res[0].name == 'Яйцо'
+
+    sl.remove_recipe('Суп')
+
+    res2 = sl.get_list()
+    assert len(res2) == 1
+
+def test_shoppinglist_get_list():
+    '''Проверка метода get_list'''
+    recipe1 = Recipe('Блины', [Ingredient('Мука', 200, 'г'), Ingredient('Яйцо', 2, 'шт')])
+    recipe2 = Recipe('Пирог', [Ingredient('Мука', 300, 'г'), Ingredient('Сахар', 100, 'г')])
+    
+    sl = ShoppingList()
+    sl.add_recipe(recipe1, 1)
+    sl.add_recipe(recipe2, 1)
+
+    res = sl.get_list()
+
+    assert len(res) == 3
+    assert res[0].name == 'Мука'
+    assert res[0].quantity == 500.0
+    assert res[1].name == 'Сахар'
+    assert res[2].name == 'Яйцо'
+
+def test_shoppinglist_add():
+    '''Проверка метода __add__'''
+    recipe1 = Recipe('Чай', [Ingredient('Вода', 200, 'мл')])
+    recipe2 = Recipe('Пирог', [Ingredient('Мука', 300, 'г')])
+
+    sl1 = ShoppingList()
+    sl1.add_recipe(recipe1, 1)
+
+    sl2 = ShoppingList()
+    sl2.add_recipe(recipe2, 1)
+
+    sl3 = sl1 + sl2 
+
+    assert len(sl3.get_list()) == 2
+    
+    assert len(sl1.get_list()) == 1
+    assert len(sl2.get_list()) == 1
